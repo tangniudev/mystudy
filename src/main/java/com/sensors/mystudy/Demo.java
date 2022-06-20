ae27a4c0250e31bf813431cae19414091a79e14b
tnn <2868491037@qq.com>
2022-06-20 15:28:13 +0800

    first commit

---
+++ src/main/java/com/sensors/mystudy/Demo.java
@@ -0,0 +1,13 @@
+package com.sensors.mystudy;
+
+/**
+ * Description
+ *
+ * @author tyw
+ * @since 2022/6/20
+ */
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("========");
+    }
+}
